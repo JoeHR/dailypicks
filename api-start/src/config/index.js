8f4738190a1a6b164b8c4ebf209da960d80d6c6f
rh <raoheng@zailingtech.com>
2020-05-07 23:11:42 +0800

    UPDATE

--- api-start/src/config/index.js
+++ api-start/src/config/index.js
@@ -5,7 +5,11 @@ const REDIS = {
   password: '123456',
 }
 
+const JWT_SECRET =
+  'a&*38QthAKuiRwISGLotgq^3%^$zvA3A6Hfr8MF$jM*HY4*dWcwAW&9NGp7*b53!'
+
 export default {
   DB_URL,
   REDIS,
+  JWT_SECRET,
 }
--- front-start/src/views/Login.vue
+++ front-start/src/views/Login.vue
@@ -8,22 +8,14 @@
             <router-link :to="{ name: 'reg' }">注册</router-link>
           </li>
         </ul>
-        <div
-          class="layui-form layui-tab-content"
-          id="LAY_ucm"
-          style="padding: 20px 0;"
-        >
+        <div class="layui-form layui-tab-content" id="LAY_ucm" style="padding: 20px 0;">
           <validation-observer ref="observer" v-slot="{ validate }">
             <div class="layui-tab-item layui-show">
               <div class="layui-form layui-form-pane">
                 <form method="post">
                   <div class="layui-form-item">
                     <label for="L_email" class="layui-form-label">用户名</label>
-                    <validation-provider
-                      name="email"
-                      rules="required|email"
-                      v-slot="{ errors }"
-                    >
+                    <validation-provider name="email" rules="required|email" v-slot="{ errors }">
                       <div class="layui-input-inline">
                         <input
                           type="text"
@@ -41,11 +33,7 @@
                   </div>
                   <div class="layui-form-item">
                     <label for="L_pass" class="layui-form-label">密码</label>
-                    <validation-provider
-                      name="password"
-                      rules="required|min:6"
-                      v-slot="{ errors }"
-                    >
+                    <validation-provider name="password" rules="required|min:6" v-slot="{ errors }">
                       <div class="layui-input-inline">
                         <input
                           type="password"
@@ -62,15 +50,9 @@
                     </validation-provider>
                   </div>
                   <div class="layui-form-item">
-                    <validation-provider
-                      name="code"
-                      rules="required|length:4"
-                      v-slot="{ errors }"
-                    >
+                    <validation-provider name="code" rules="required|length:4" v-slot="{ errors }">
                       <div class="layui-row">
-                        <label for="L_vercode" class="layui-form-label"
-                          >验证码</label
-                        >
+                        <label for="L_vercode" class="layui-form-label">验证码</label>
                         <div class="layui-input-inline">
                           <input
                             type="text"
@@ -82,12 +64,7 @@
                           />
                         </div>
                         <div class>
-                          <span
-                            class="svg"
-                            style="color: #c00;"
-                            @click="_getCode()"
-                            v-html="svg"
-                          ></span>
+                          <span class="svg" style="color: #c00;" @click="_getCode()" v-html="svg"></span>
                         </div>
                       </div>
                       <div class="layui-form-mid">
@@ -96,13 +73,9 @@
                     </validation-provider>
                   </div>
                   <div class="layui-form-item">
-                    <button class="layui-btn" @click="validate().then(submit)">
-                      立即登录
-                    </button>
+                    <button class="layui-btn" type="button" @click="validate().then(submit)">立即登录</button>
                     <span style="padding-left:20px;">
-                      <router-link :to="{ name: 'forget' }"
-                        >忘记密码？</router-link
-                      >
+                      <router-link :to="{ name: 'forget' }">忘记密码？</router-link>
                     </span>
                   </div>
                   <div class="layui-form-item fly-form-app">
@@ -173,9 +146,9 @@ export default {
     async submit() {
       const isValid = await this.$refs.observer.validate();
       if (!isValid) {
-        return;
+        return false;
       }
-      debugger;
+      console.log(1111111)
       login({
         username: this.username,
         password: this.password,
