804a717baed2b5281c4d3cb910a8781aee1c00e0
rh <raoheng@zailingtech.com>
2020-05-21 20:29:53 +0800

    学习了解 layui中的栅格系统

--- front-start/src/router.js
+++ front-start/src/router.js
@@ -1,35 +1,41 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
 
-const Login = () => import(/* webpackChunkName: 'login' */ "./views/Login.vue");
-const Reg = () => import(/* webpackChunkName: 'reg' */ "./views/Reg.vue");
+const Login = () => import(/* webpackChunkName: 'login' */ './views/Login.vue');
+const Reg = () => import(/* webpackChunkName: 'reg' */ './views/Reg.vue');
 const Forget = () =>
-  import(/* webpackChunkName: 'forget' */ "./views/Forget.vue");
+  import(/* webpackChunkName: 'forget' */ './views/Forget.vue');
+const Home = () => import('@/views/Home.vue');
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/login",
-      name: "login",
+      path: '/',
+      name: 'index',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
       component: Login
     },
     {
-      path: "/reg",
-      name: "reg",
+      path: '/reg',
+      name: 'reg',
       component: Reg,
       beforeEnter: (to, from, next) => {
-        if (from.name === "login") {
+        if(from.name === 'login') {
           next();
         } else {
-          next("/login");
+          next('/login');
         }
       }
     },
     {
-      path: "/forget",
-      name: "forget",
+      path: '/forget',
+      name: 'forget',
       component: Forget
     }
   ]
